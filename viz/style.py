77e8422532ebbd32477a9dc58164fbb651e299b5
Jack Yordy <yordyjv@appstate.edu>
2025-06-03 18:22:56 -0400

    Added style

---
+++ viz/style.py
@@ -0,0 +1,17 @@
+# viz/style.py
+
+# Standard party color mapping
+PARTY_COLORS = {
+    "DEM": "#3366cc",        # Democrat = Blue
+    "REP": "#dc3912",        # Republican = Red
+    "UNA": "#888888",        # Unaffiliated = Gray
+    "LIB": "#ff9900",        # Libertarian = Orange
+    "GRE": "#109618",        # Green = Green
+}
+
+# General style settings
+DEFAULT_FONT = "Arial"
+DEFAULT_FONT_SIZE = 12
+TITLE_FONT_SIZE = 16
+AXIS_FONT_SIZE = 12
+MARGIN = 0.1
